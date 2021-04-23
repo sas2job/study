3a150b336e6cd6055e0b4609e4182258bfdbd25f
Alex Smirnov <sas2job@gmail.com>
2021-04-23 22:04:11 +0300

    Add solution for fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,35 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for item in self
+          yield item
+        end
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        new_array = MyArray.new
+        my_each { |item| new_array << yield(item) }
+        new_array
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        new_array = MyArray.new
+        my_each { |item| new_array << item unless item.nil? }
+        new_array
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial = nil)
+        acc = initial
+        my_each do |item|
+          acc = acc.nil? ? item : yield(acc, item)
+        end
+        acc
+      end
     end
   end
 end
